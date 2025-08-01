dba65c4e465d64bfda2e25030914cba84fd20e0a
Dharmik B. <dharmikbhuva1818@gmail.com>
2025-08-01 20:55:13 +0530

    ok

---
+++ 4. JS/dom/change-color/app.js
@@ -0,0 +1,11 @@
+/**
+ * Write your challenge solution here
+ */
+//  greenButton
+// blueButton
+// purpleButton
+// resetButton
+
+const buttons = document.querySelectorAll('color-buttons')
+const mainHeading = document.getElementById('mainHeading');
+
--- 5. NODE/FULLSTACK/routes/user.routes.js
+++ 5. NODE/FULLSTACK/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser } from "../controller/user.controller.js";
+import { registerUser } from "../controller/User.controller.js";
 
 
 const router = express.Router();
